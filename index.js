1ee2d45974f074de56ede842bbcb3d54113c6cf4
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-11-27 23:20:07 +0800

    tags check & test

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+var tags = {
+  title: 'ti',
+  artist: 'ar',
+  author: 'au',
+  album: 'al',
+  length: 'length',
+  offset: 'offset',
+  by: 'by',
+  resource: 're',
+  version: 've'
+};
+
 module.exports = function(str) {
   if (/\[(\d{2,})\:(\d{2})(?:\.(\d{2}))?\]/g.test(str)) {
     return true;
   };
-
+  for (var key in tags) {
+    if ((new RegExp('\\[' + tags[key] + ':([^\\]]*)\\]', 'i')).test(str)) {
+      return true;
+    };
+  }
   return false;
 };
